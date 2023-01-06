7f1e905ec4163f518e9f3bc45f466898633fc36b
Francis Wibisono <code@franciswibisono.com>
2023-01-06 20:42:43 +0700

    added boilerplate netlify function, add basic ui colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#1b1c1e',
+        text: '#f5f5f5',
+        'accent-1': '#1691cc',
+        'accent-2': '#ff9f1c',
+      }
+    },
   },
   plugins: [],
 }
